e55344db8c459e98d8ac030a9e9d347cd04a7044
David Waring <dev@davidwaring.net>
2017-11-03 11:01:50 -0400

    Add directory exports

---
+++ src/request/index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = {
+  auth: require('./auth.js'),
+  get: require('./get.js'),
+  sign: require('./sign.js')
+};
---
+++ src/response/index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = {
+  error: require('./error.js'),
+  parse: require('./parse.js'),
+  response: require('./response.js'),
+  success: require('./success.js')
+};
