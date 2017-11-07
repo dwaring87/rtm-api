0c50eb2743ced6199b1c94e819b533d2f2ec8ad1
David Waring <dev@davidwaring.net>
2017-11-07 13:58:53 -0500

    API Helpers: lists

---
+++ src/lists/helper.js
@@ -0,0 +1,118 @@
+'use strict';
+
+const RTMList = require('./list.js');
+const genIndex = require('../utils/genIndex.js');
+
+
+// ==== LISTS API CALL HELPERS ==== //
+
+
+/**
+ * API Call: rtm.lists.getList & update user lists
+ * @param user RTMUser
+ * @param callback Callback function(err, lists)
+ * @private
+ */
+function get(user, callback) {
+  user.get('rtm.lists.getList', function(resp) {
+    if ( !resp.isOk ) {
+      return callback(resp);
+    }
+
+    // List of lists to return
+    let rtn = [];
+
+    // Parse each of the lists
+    let lists = resp.lists.list;
+    for ( let i = 0; i < lists.length; i++ ) {
+      rtn.push(
+        new RTMList(lists[i])
+      );
+    }
+
+    // Add indices to lists
+    rtn = genIndex(rtn);
+
+    // Set user lists
+    user._lists = rtn;
+
+    // Call the callback
+    return callback(null, rtn);
+  });
+}
+
+
+/**
+ * API Call: rtm.lists.add & update user lists
+ * @param name RTM List Name
+ * @param user RTMUser
+ * @param callback Callback function(err, lists)
+ * @private
+ */
+function add(name, user, callback) {
+  if ( name === 'Inbox' || name === 'Sent' ) {
+    throw "Invalid List Name"
+  }
+  let params = {
+    name: name,
+    timeline: user.timeline
+  };
+  user.get('rtm.lists.add', params, function(resp) {
+    if ( !resp.isOk ) {
+      return callback(resp);
+    }
+    return get(user, callback);
+  });
+}
+
+/**
+ * API Call: rtm.lists.delete & update user lists
+ * @param id RTM List ID
+ * @param user RTMUser
+ * @param callback Callback function(err, lists)
+ * @private
+ */
+function remove(id, user, callback) {
+  let params = {
+    timeline: user.timeline,
+    list_id: id
+  };
+  user.get('rtm.lists.delete', params,  function(resp) {
+    if ( !resp.isOk ) {
+      return callback(resp);
+    }
+    return get(user, callback);
+  });
+}
+
+/**
+ * API Call: rtm.lists.setName & update user lists
+ * @param id RTM List ID
+ * @param name New RTM List Name
+ * @param user RTMUser
+ * @param callback Callback function(err, lists)
+ * @private
+ */
+function rename(id, name, user, callback) {
+  let params = {
+    timeline: user.timeline,
+    list_id: id,
+    name: name
+  };
+  user.get('rtm.lists.setName', params, function(resp) {
+    if ( !resp.isOk ) {
+      return callback(resp);
+    }
+    return get(user, callback);
+  });
+}
+
+
+
+
+module.exports = {
+  get: get,
+  add: add,
+  remove: remove,
+  rename: rename
+};
---
+++ src/lists/list.js
@@ -0,0 +1,141 @@
+'use strict';
+
+
+/**
+ * ### RTM List
+ *
+ * This Class is used to represent the properties of an RTM List.
+ *
+ * All of the list properties from RTM are directly accessible from this Class.
+ *
+ * ```
+ * let list = new RTMList(...);
+ * let name = list.name;
+ * ```
+ * @class
+ */
+class RTMList {
+
+  /**
+   * Create a new RTM List
+   * @param {object} props The properties from the RTM API response `resp.lists.list`
+   */
+  constructor(props) {
+
+    this._index = undefined;
+    this.id = undefined;
+    this.name = undefined;
+
+    // Parse the list properties
+    for ( let key in props ) {
+      if ( props.hasOwnProperty(key) ) {
+        let value = props[key];
+        if ( key === 'id' ) {
+          value = parseFloat(value);
+        }
+        if ( key === 'position' || key === 'sort_order' ) {
+          value = parseInt(value);
+        }
+        else {
+          if ( value === '0' ) {
+            value = false;
+          }
+          else if ( value === '1' ) {
+            value = true;
+          }
+        }
+        this[key] = value;
+      }
+    }
+
+  }
+
+  /**
+   * An index added to each RTM List based on its `id`
+   * @returns {int}
+   */
+  get index() {
+    return this._index;
+  }
+
+  /**
+   * All of the RTM List properties
+   * @returns {object}
+   */
+  get props() {
+    let rtn = {};
+    for ( let key in this ) {
+      if ( this.hasOwnProperty(key) && key !== '_index' ) {
+        rtn[key] = this[key];
+      }
+    }
+    return rtn;
+  }
+
+}
+
+
+
+
+
+// ==== SORT FUNCTIONS ==== //
+
+
+/**
+ * Sort the RTM Lists by the rtm-api added index field
+ * @param {RTMList} a RTM List a
+ * @param {RTMList} b RTM List b
+ * @returns {number}
+ */
+RTMList.sortByIndex = function(a, b) {
+  if ( parseInt(a.index) < parseInt(b.index) ) {
+    return -1;
+  }
+  else if ( parseInt(a.index) > parseInt(b.index) ) {
+    return 1;
+  }
+  else {
+    return 0;
+  }
+};
+
+
+/**
+ * Sort the RTM Lists by the RTM List id
+ * @param {RTMList} a RTM List a
+ * @param {RTMList} b RTM List b
+ * @returns {number}
+ */
+RTMList.sortById = function(a, b) {
+  if ( a.id < b.id ) {
+    return -1;
+  }
+  else if ( a.id > b.id ) {
+    return 1;
+  }
+  else {
+    return 0;
+  }
+};
+
+
+/**
+ * Sort the RTM Lists by the RTM List name
+ * @param {RTMList} a RTM List a
+ * @param {RTMList} b RTM List b
+ * @returns {number}
+ */
+RTMList.sortByName = function(a, b) {
+  if ( a.name.toLowerCase() < b.name.toLowerCase() ) {
+    return -1;
+  }
+  else if ( a.name.toLowerCase() > b.name.toLowerCase() ) {
+    return 1;
+  }
+  else {
+    return 0;
+  }
+};
+
+
+module.exports = RTMList;
