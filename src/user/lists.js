3a8e4edd12b773e8efc6ef4f2bea7ab50f5635d0
David Waring <dev@davidwaring.net>
2017-11-12 15:47:29 -0500

    Lists: rename & remove by name
    reference lists by name and not index number

--- src/user/lists.js
+++ src/user/lists.js
@@ -17,7 +17,7 @@ module.exports = function(user) {
    * Get the list of RTM Lists for this User from the API Server
    * @param {function} callback Callback function(err, lists)
    * @param {RTMError} callback.err RTM API Error Response, if encountered
-   * @param {RTMList[]} callback.list List of User's RTM Lists
+   * @param {RTMList[]} callback.lists List of User's RTM Lists
    * @function RTMUser~lists/get
    */
   rtn.get = function(callback) {
@@ -27,9 +27,8 @@ module.exports = function(user) {
   /**
    * Add a new RTM List for this User
    * @param {string} name Name of the new RTM List
-   * @param {function} callback Callback function(err, list)
+   * @param {function} callback Callback function(err, lists)
    * @param {RTMError} callback.err RTM API Error Response, if encountered
-   * @param {RTMList[]} callback.list List of User's RTM Lists
    * @function RTMUser~lists/add
    */
   rtn.add = function(name, callback) {
@@ -38,59 +37,74 @@ module.exports = function(user) {
 
   /**
    * Remove the specified RTM List for this User
-   * @param {int} index RTM List index
-   * @param {function} callback Callback function(err, list)
+   * @param {string} name RTM List Name
+   * @param {function} callback Callback function(err)
    * @param {RTMError} callback.err RTM API Error Response, if encountered
-   * @param {RTMList[]} callback.list List of User's RTM Lists
    * @function RTMUser~lists/remove
    */
-  rtn.remove = function(index, callback) {
-    let found = false;
-    if ( user._lists ) {
-      for ( let i = 0; i < user._lists.length; i++ ) {
-        if ( user._lists[i].index === index ) {
-          found = true;
-          _lists.remove(user._lists[i].id, user, function(err) {
-            if ( err ) {
-              return callback(err);
-            }
-            return rtn.update(callback);
-          });
+  rtn.remove = function(name, callback) {
+
+    // Get the User's Lists
+    _lists.get(user, function(err, lists) {
+      if ( err ) {
+        return callback(err);
+      }
+
+      // Find the matching list IDs
+      let ids = [];
+      for ( let i = 0; i < lists.length; i++ ) {
+        if ( lists[i].name.toLowerCase() === name.toLowerCase() ) {
+          ids.push(lists[i].id);
         }
       }
-    }
-    if ( !found ) {
-      return callback(errors.indexError());
-    }
+
+      // Remove the matching List
+      if ( ids.length === 1 ) {
+        return _lists.remove(ids[0], user, callback);
+      }
+      else {
+        return callback(errors.referenceError());
+      }
+
+    });
+
   };
 
   /**
    * Rename the specified RTM List for this User
-   * @param {int} index RTM List index
-   * @param {string} name New RTM List name
+   * @param {string} oldName Old RTM List Name
+   * @param {string} newName New RTM List name
    * @param {function} callback Callback function(err, list)
    * @param {RTMError} callback.err RTM API Error Response, if encountered
    * @param {RTMList[]} callback.list List of User's RTM Lists
    * @function RTMUser~lists/rename
    */
-  rtn.rename = function(index, name, callback) {
-    let found = false;
-    if ( user._lists ) {
-      for ( let i = 0; i < user._lists.length; i++ ) {
-        if ( user._lists[i].index === index ) {
-          found = true;
-          _lists.rename(user._lists[i].id, name, user, function(err) {
-            if ( err ) {
-              return callback(err);
-            }
-            return rtn.update(callback);
-          });
+  rtn.rename = function(oldName, newName, callback) {
+
+    // Get the User's Lists
+    _lists.get(user, function(err, lists) {
+      if ( err ) {
+        return callback(err);
+      }
+
+      // Find the matching list IDs
+      let ids = [];
+      for ( let i = 0; i < lists.length; i++ ) {
+        if ( lists[i].name.toLowerCase() === oldName.toLowerCase() ) {
+          ids.push(lists[i].id);
         }
       }
-    }
-    if ( !found ) {
-      return callback(errors.indexError());
-    }
+
+      // Rename the matching List
+      if ( ids.length === 1 ) {
+        return _lists.rename(ids[0], newName, user, callback);
+      }
+      else {
+        return callback(errors.referenceError());
+      }
+
+    });
+
   };
 
   return rtn;
